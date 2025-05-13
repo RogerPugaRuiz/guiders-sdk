37a99aea234a3789c70f6c56275bf30fece5b013
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-05-13 08:47:34 +0200

    feat(tracking): actualizar la inicialización del SDK y corregir la asignación de la variable global

--- demo/app/partials/header.php
+++ demo/app/partials/header.php
@@ -14,7 +14,7 @@
   <script>
     const sdk = new window.TrackingPixelSDK({
       // endpoint: "https://guiders-backend-production.up.railway.app",
-      apiKey: "49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d9763",
+      apiKey: "44",
       autoFlush: true,
       flushInterval: 1000, // 1 second
       maxRetries: 2,
@@ -24,7 +24,7 @@
     (async () => {
       await sdk.init();
       sdk.enableDOMTracking();
-      window.sdk = sdk;
+      window.guiders = sdk;
     })();
 
   </script>
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export * from "./types";
 declare global {
 	interface Window {
 		TrackingPixelSDK: typeof TrackingPixelSDK;
+		guiders: TrackingPixelSDK;
 	}
 }
 
@@ -22,5 +23,17 @@ if (typeof window !== "undefined") {
 	// Para evitar problemas de módulos, asegúrate de que estas importaciones
 	// se correspondan con la salida compilada de tu proyecto.
 	window.TrackingPixelSDK = TrackingPixelSDK;
+	window.guiders = new window.TrackingPixelSDK({
+		// endpoint: "https://guiders-backend-production.up.railway.app",
+		apiKey: "49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d9763",
+		autoFlush: true,
+		flushInterval: 1000, // 1 second
+		maxRetries: 2,
+	});
 
+	window.guiders.init();
+	(async () => {
+		await window.guiders.init();
+		window.guiders.enableDOMTracking();
+	})();
 }
