074b9f717eab46b3c04fc192e2553a8681007014
RogerPugaRuiz <rogerpugaruiz@gmail.com>
2025-06-23 08:46:43 +0200

    feat: Agregar archivos de demostración para el seguimiento de sesiones

--- demo/app/partials/header.php
+++ demo/app/partials/header.php
@@ -13,7 +13,7 @@
   <script src="http://127.0.0.1:8081/index.js?apiKey=49960de5880e8c687434170f6476605b8fe4aeb9a28632c7995cf3ba831d9763"></script>
    
   <!-- Producción: -->
-  <!-- <script src="https://guiders-sdk.s3.eu-north-1.amazonaws.com/0.0.1/index.js?apiKey=762999137d6431e90eb31a35ba3c97eef6f7856af625ece2cade228ff29b85c1"></script> -->
+  <!-- <script src="https://guiders-sdk.s3.eu-north-1.amazonaws.com/0.0.1/index.js?apiKey=0dfcbca086b2ca8ef8000e15d10aa9b5efc40b2f8591b1bfa36725e3848f0e0a"></script> -->
 
   <script>
     (function() {
